6f28e13e2cd72005ef09feeb58b23a8860e9cacc
abrar zahin <abrarzahin@yahoo.com>
2019-08-04 16:21:18 -0400

    activity detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'activity-detail', loadChildren: './activity-detail/activity-detail.module#ActivityDetailPageModule' }
 ];
 @NgModule({
   imports: [

8d789c06c94fa0f903408f06eb9cecf926ee8d84
abrar zahin <abrarzahin@yahoo.com>
2019-08-11 22:27:38 -0400

    auth service created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { RouteReuseStrategy } from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 
 import {AngularFireModule} from "@angular/fire";
+import {AngularFireAuthModule} from "@angular/fire/auth";
 import {environment} from "../environments/environment";
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -21,6 +22,7 @@ import { AppComponent } from './app.component';
   entryComponents: [],
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
     BrowserModule,
     HttpClientModule,
      IonicModule.forRoot(),

2889ad16fa8e7fa3e8eb81bbd0b68272d97701fb
abrar zahin <abrarzahin@yahoo.com>
2019-08-25 20:00:28 -0400

    favorites displayed

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+ favoriteActivityList: Observable<any>;
+
+ constructor(
+  private _angularFireStore: AngularFirestore,
+  private _angularFireAuth: AngularFireAuth,
+){
+  this.favoriteActivityList = _angularFireStore
+    .collection("favorites")
+    .doc(_angularFireAuth.auth.currentUser.uid)
+    .collection("favorites")
+    .valueChanges();
+}
+
+
+
+}
